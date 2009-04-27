23d4cf16e00494dfd238307ec99c776b224cdd5d
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-27 06:21:38 +0000

    Fixed problem with frames in Gecko browsers
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/llxmltranslate/trunk@19491 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,7 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 if (TYPO3_MODE=='BE')	{
 	t3lib_extMgm::addModule('txllxmltranslateM1','','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 	t3lib_extMgm::addLLrefForTCAdescr('_MOD_txllxmltranslateM1','EXT:llxmltranslate/locallang_csh.xml');
+	t3lib_extMgm::addModule('txllxmltranslateM1','txllxmltranslateM1','bottom',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 }
+
 ?>
