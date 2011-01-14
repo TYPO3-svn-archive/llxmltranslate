9f910d91cfdaa584b1ee821a1e3fafd4513eca33
king76 <king76@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-14 22:49:39 +0000

    Move LL-XML module after "user" module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/llxmltranslate/trunk@42147 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,7 +2,24 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 if (TYPO3_MODE=='BE')	{
-	t3lib_extMgm::addModule('user','txllxmltranslateM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+	
+	// add module after 'Web'
+	if (!isset($TBE_MODULES['txllxml']))	{
+		$temp_TBE_MODULES = array();
+		foreach($TBE_MODULES as $key => $val) {
+			if ($key === 'user') {
+				$temp_TBE_MODULES[$key] = $val;
+				$temp_TBE_MODULES['txllxml'] = $val;
+			} else {
+				$temp_TBE_MODULES[$key] = $val;
+			}
+		}
+		$TBE_MODULES = $temp_TBE_MODULES;
+		unset($temp_TBE_MODULES);
+	}
+
+	t3lib_extMgm::addModule('txllxml','','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+	t3lib_extMgm::addModule('txllxml','translate','bottom',t3lib_extMgm::extPath($_EXTKEY).'mod2/');
 	t3lib_extMgm::addLLrefForTCAdescr('_MOD_txllxmltranslateM1','EXT:llxmltranslate/locallang_csh.xml');
 }
 ?>
--- mod1/conf.php
+++ mod1/conf.php
@@ -3,11 +3,7 @@
 // DO NOT REMOVE OR CHANGE THESE 3 LINES:
 define('TYPO3_MOD_PATH', '../typo3conf/ext/llxmltranslate/mod1/');
 $BACK_PATH='../../../../typo3/';
-$MCONF['name'] = 'user_txllxmltranslateM1';
+$MCONF['name'] = 'txllxml';
 
-$MCONF['access'] = 'user,group';
-$MCONF['script'] = 'index.php';
-
-$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:llxmltranslate/mod1/locallang_mod.xml';
 ?>
