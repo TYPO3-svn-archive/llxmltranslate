2f8ef13e5aaf262fd224f59a2d9d8c0267fc3a39
king76 <king76@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-14 13:44:56 +0000

    New icon and move as sub-module in "user" module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/llxmltranslate/trunk@42118 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/conf.php
+++ mod1/conf.php
@@ -3,8 +3,7 @@
 	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
 define('TYPO3_MOD_PATH', '../typo3conf/ext/llxmltranslate/mod1/');
 $BACK_PATH='../../../../typo3/';
-$MCONF['name'] = 'txllxmltranslateM1';
-
+$MCONF['name'] = 'user_txllxmltranslateM1';
 
 $MCONF['access'] = 'user,group';
 $MCONF['script'] = 'index.php';
