e1b5ab08f9ab2139e3be599d5c1dfeffefd8bdde
king76 <king76@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-14 23:03:19 +0000

    Move LL-XML module after "user" module (mod2)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/llxmltranslate/trunk@42148 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ mod2/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/llxmltranslate/mod2/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['name'] = 'txllxml_translate';
+
+$MCONF['access'] = 'user,group';
+$MCONF['script'] = 'index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:llxmltranslate/mod2/locallang_mod.xml';
+?>
---
+++ mod2/index.php
@@ -0,0 +1,91 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2004 Kasper Skaarhoj (kasper@typo3.com)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Module 'll-XML' for the 'llxmltranslate' extension.
+ *
+ * @author	Kasper Skaarhoj <kasper@typo3.com>
+ */
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *  100: class tx_llxmltranslate_module1 extends t3lib_SCbase
+ *  121:     function init()
+ *  130:     function menuConfig()
+ *  213:     function main()
+ *  232:     function jumpToUrl(URL)
+ *  253:     function printContent()
+ *  263:     function moduleContent()
+ *
+ *              SECTION: Rendering
+ *  324:     function renderSettings()
+ *  355:     function renderTranslate()
+ *  386:     function renderExport()
+ *  467:     function renderMerge()
+ *
+ *              SECTION: Create form fields etc.
+ *  517:     function createEditForm($relFileRef,$saveStatus=TRUE)
+ *  680:     function createEditForm_getItemRow($xmlArray, $relFileRef, $labelKey, $alwaysTextarea=FALSE)
+ *  861:     function createEditForm_getItemHead()
+ *  891:     function createMergeForm()
+ *
+ *              SECTION: Saving and getting XML content
+ * 1049:     function saveSubmittedData()
+ * 1188:     function getXMLdata($fileRef)
+ * 1229:     function saveXMLdata($fileRef,$xmlArray, &$saveLog, &$errorLog)
+ * 1348:     function createXML($outputArray)
+ *
+ *              SECTION: Helper functions
+ * 1389:     function getllxmlFiles()
+ * 1421:     function getllxmlFiles_cached($regenerate=FALSE)
+ * 1442:     function loadTranslationStatus($files=FALSE)
+ * 1468:     function openCSH($p)
+ * 1479:     function llxmlFileInfoBox($xmlArray,$relFileRef)
+ *
+ * TOTAL FUNCTIONS: 23
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+
+// DEFAULT initialization of a module [BEGIN]
+unset($MCONF);
+require('conf.php');
+require($BACK_PATH.'init.php');
+
+// Force Frame to utf8 charset
+$TYPO3_CONF_VARS['BE']['forceCharset'] = 'utf-8';
+
+require(t3lib_extMgm::extPath('llxmltranslate').'mod2/class.sc.php');
+$BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
+
+// Include language file
+$LANG->includeLLFile('EXT:llxmltranslate/mod2/locallang.xml');
+
+// Make instance:
+$SOBE = t3lib_div::makeInstance('tx_llxmltranslate_module1');
+$SOBE->init();
+$SOBE->main();
+$SOBE->printContent();
+?>
