207871ffb17eb6e3b54ccb6f027f31690534e75e
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-13 18:09:41 +0000

    [BUGFIX] #42939 Drop the function index
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/llxmltranslate/trunk@67827 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod2/index.php
+++ mod2/index.php
@@ -21,53 +21,6 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
-/**
- * Module 'll-XML' for the 'llxmltranslate' extension.
- *
- * @author	Kasper Skaarhoj <kasper@typo3.com>
- */
-/**
- * [CLASS/FUNCTION INDEX of SCRIPT]
- *
- *
- *
- *  100: class tx_llxmltranslate_module1 extends t3lib_SCbase
- *  121:     function init()
- *  130:     function menuConfig()
- *  213:     function main()
- *  232:     function jumpToUrl(URL)
- *  253:     function printContent()
- *  263:     function moduleContent()
- *
- *              SECTION: Rendering
- *  324:     function renderSettings()
- *  355:     function renderTranslate()
- *  386:     function renderExport()
- *  467:     function renderMerge()
- *
- *              SECTION: Create form fields etc.
- *  517:     function createEditForm($relFileRef,$saveStatus=TRUE)
- *  680:     function createEditForm_getItemRow($xmlArray, $relFileRef, $labelKey, $alwaysTextarea=FALSE)
- *  861:     function createEditForm_getItemHead()
- *  891:     function createMergeForm()
- *
- *              SECTION: Saving and getting XML content
- * 1049:     function saveSubmittedData()
- * 1188:     function getXMLdata($fileRef)
- * 1229:     function saveXMLdata($fileRef,$xmlArray, &$saveLog, &$errorLog)
- * 1348:     function createXML($outputArray)
- *
- *              SECTION: Helper functions
- * 1389:     function getllxmlFiles()
- * 1421:     function getllxmlFiles_cached($regenerate=FALSE)
- * 1442:     function loadTranslationStatus($files=FALSE)
- * 1468:     function openCSH($p)
- * 1479:     function llxmlFileInfoBox($xmlArray,$relFileRef)
- *
- * TOTAL FUNCTIONS: 23
- * (This index is automatically created/updated by the extension "extdeveval")
- *
- */
 
 // DEFAULT initialization of a module [BEGIN]
 unset($MCONF);
